cd02007c438a5f1bc918891d784c2c035144a3e0
Nazar Alwi <alwinazar75@gmail.com>
2021-09-02 11:45:42 +0700

    Render comments

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -43,6 +43,30 @@ extension ListViewController {
 }
 
 extension ListViewController {
+	func numberOfRenderedComments() -> Int {
+		numberOfRows(in: commentsSection)
+	}
+
+	func commentUsername(at row: Int) -> String? {
+		commentView(at: row)?.usernameLabel.text
+	}
+
+	func commentDate(at row: Int) -> String? {
+		commentView(at: row)?.dateLabel.text
+	}
+
+	func commentMessage(at row: Int) -> String? {
+		commentView(at: row)?.messageLabel.text
+	}
+
+	func commentView(at row: Int) -> ImageCommentCell? {
+		cell(row: row, section: commentsSection) as? ImageCommentCell
+	}
+
+	private var commentsSection: Int { 0 }
+}
+
+extension ListViewController {
 	@discardableResult
 	func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
 		return feedImageView(at: index) as? FeedImageCell
--- EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -4,7 +4,7 @@
 
 import UIKit
 
-class ImageCommentCell: UITableViewCell {
+public class ImageCommentCell: UITableViewCell {
 	@IBOutlet private(set) public var usernameLabel: UILabel!
 	@IBOutlet private(set) public var dateLabel: UILabel!
 	@IBOutlet private(set) public var messageLabel: UILabel!
