9b219fb0649e52d05f58a752848a077b1333b8a1
Nazar Alwi <alwinazar75@gmail.com>
2021-09-02 09:25:10 +0700

    Set comments title

--- EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -9,12 +9,12 @@ import EssentialFeed
 import EssentialFeediOS
 
 class CommentsUIIntegrationTests: FeedUIIntegrationTests {
-	override func test_feedView_hasTitle() {
+	func test_commentsView_hasTitle() {
 		let (sut, _) = makeSUT()
 
 		sut.loadViewIfNeeded()
 
-		XCTAssertEqual(sut.title, feedTitle)
+		XCTAssertEqual(sut.title, commentsTitle)
 	}
 
 	override func test_imageSelection_notifiesHandler() {
