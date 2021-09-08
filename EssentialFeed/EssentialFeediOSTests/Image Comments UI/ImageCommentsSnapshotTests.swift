53a2b36b2b4fa8eddf10e1f8623bfa1c68ca2cc8
Nazar Alwi <alwinazar75@gmail.com>
2021-09-08 21:47:37 +0700

    Remove `file` and `line` parameters since this `makeSUT()` function doesn't need those parameters

--- EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -19,7 +19,7 @@ class ImageCommentsSnapshotTests: XCTestCase {
 
 	// MARK: - Helpers
 
-	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> ListViewController {
+	private func makeSUT() -> ListViewController {
 		let bundle = Bundle(for: ListViewController.self)
 		let storyboard = UIStoryboard(name: "ImageComments", bundle: bundle)
 		let controller = storyboard.instantiateInitialViewController() as! ListViewController
